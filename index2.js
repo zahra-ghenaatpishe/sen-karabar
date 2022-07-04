95a25cd987ae643923edd42715a6ebebf34527d8
zahra-ghenaatpishe <108525052+zahra-ghenaatpishe@users.noreply.github.com>
2022-07-05 00:32:32 +0430

    tamrin 2

---
+++ index2.js
@@ -0,0 +1,14 @@
+
+const birthday = prompt("enter your birthday");
+const currentAge  =2022 - birthday;
+console.log(currentAge);
+if ( currentAge==18 ){
+   console.log('sen shoma beyn 18 ta 20 ast')
+} else if(currentAge==19){
+    console.log('sen shoma beyn 18 ta 20 ast')
+}else if(currentAge==20){
+    console.log('sen shoma beyn 18 ta 20 ast')
+}
+const ageBad = currentAge + 56;
+console.log('sen shoma bad az 56 sal!');
+console.log(ageBad);
